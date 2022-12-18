d3614df68930fafcf9e6d631fff5b6895aac9aa9
Salomo Bartholomees <salomob@gmail.com>
2022-12-18 16:09:21 +0100

    change keybindings for telescope

--- mo-vim/.config/nvim/lua/user/telescope.lua
+++ mo-vim/.config/nvim/lua/user/telescope.lua
@@ -3,7 +3,9 @@ if not status_ok then
   return
 end
 
--- local actions = require "telescope.actions"
+local actions = require "telescope.actions"
+
+pcall(require('telescope').load_extension, 'fzf')
 
 telescope.setup {
   defaults = {
@@ -11,25 +13,26 @@ telescope.setup {
     prompt_prefix = " ",
     selection_caret = " ",
     path_display = { "smart" },
-    file_ignore_patterns = { ".git/", "node_modules", "venv" },
-    -- layout_strategy = "horizontal",
-    -- layout_config = {
-    --   horizontal = {
-    --     mirror = false,
-    --   },
-    --   vertical = {
-    --     mirror = false,
-    --   },
-    -- },
 
-    -- mappings = {
-    --   i = {
-    --     ["<Down>"] = actions.cycle_history_next,
-    --     ["<Up>"] = actions.cycle_history_prev,
-    --     ["<C-j>"] = actions.move_selection_next,
-    --     ["<C-k>"] = actions.move_selection_previous,
-    --   },
-    -- },
+    file_ignore_patterns = { ".git/", "node_modules", "venv" },
+    layout_strategy = "horizontal",
+    layout_config = {
+      horizontal = {
+        mirror = false,
+      },
+      vertical = {
+        mirror = false,
+      },
+    },
+
+    mappings = {
+      i = {
+        ["<Down>"] = actions.cycle_history_next,
+        ["<Up>"] = actions.cycle_history_prev,
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+      },
+    },
   },
   extensions = {
     fzf = {
@@ -38,8 +41,18 @@ telescope.setup {
       override_file_sorter = true,     -- override the file sorter
       case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
                                        -- the default case_mode is "smart_case"
-    }
+    },
+    -- project = {
+    --   base_dirs = {
+    --     {"~/Documents/git", max_depth = 4},
+    --     "~/Documents",
+    --   },
+    --   hidden_files = true,
+    --   theme = "dropdown",
+    --   order_by = "asc",
+    --   sync_with_nvim_tree = true,
+    -- }
   },
 }
 -- pcall(require("telescope").load_extension, 'harpoon')
-pcall(require('telescope').load_extension, 'fzf')
+-- require'telescope'.extensions.project.project{}
