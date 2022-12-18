0362777572a5915e6b412de3a6ca7efd5f79bd5a
Salomo Bartholomees <salomob@gmail.com>
2022-12-18 15:06:59 +0100

    rewrite logic

--- mo-vim/.config/nvim/lua/user/plugins.lua
+++ mo-vim/.config/nvim/lua/user/plugins.lua
@@ -1,17 +1,9 @@
-local fn = vim.fn
-
--- Automatically install packer
-local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
-  print "Installing packer close and reopen Neovim..."
+-- Install packer
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+local is_bootstrap = false
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  is_bootstrap = true
+  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
   vim.cmd [[packadd packer.nvim]]
 end
 
@@ -41,8 +33,8 @@ packer.init {
   },
 }
 
+require('packer').startup(function(use)
 -- Install your plugins here
-return packer.startup(function(use)
   use { "wbthomason/packer.nvim", commit = "6afb67460283f0e990d35d229fd38fdc04063e0a" } -- Have packer manage itself
   -- My plugins here
   -- use { "mrjones2014/smart-splits.nvim"}
@@ -134,10 +126,26 @@ return packer.startup(function(use)
   --     ['*'] = require('distant.settings').chip_default()
   --   }
   -- end}
+-- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
+  -- local has_plugins, plugins = pcall(require, 'custom.plugins')
+  -- if has_plugins then
+  --   plugins(use)
+  -- end
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if PACKER_BOOTSTRAP then
-    require("packer").sync()
+  if is_bootstrap then
+    require('packer').sync()
   end
 end)
+
+-- When we are bootstrapping a configuration, it doesn't
+-- make sense to execute the rest of the init.lua.
+--
+-- You'll need to restart nvim, and then it will work.
+if is_bootstrap then
+  print '=================================='
+  print '    Plugins are being installed'
+  print '    Wait until Packer completes,'
+  print '       then restart nvim'
+  print '=================================='
+  return
+end
