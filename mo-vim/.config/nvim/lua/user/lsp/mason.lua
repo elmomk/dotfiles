83d69f15349e13e4be971df5465bae2dd56c3a2f
Salomo Bartholomees <salomob@gmail.com>
2023-02-08 09:07:54 +0100

    disable auto install

--- mo-vim/.config/nvim/lua/user/lsp/mason.lua
+++ mo-vim/.config/nvim/lua/user/lsp/mason.lua
@@ -45,7 +45,7 @@ local settings = {
 require("mason").setup(settings)
 require("mason-lspconfig").setup({
 	ensure_installed = servers,
-	automatic_installation = true,
+	automatic_installation = false,
 })
 
 local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
