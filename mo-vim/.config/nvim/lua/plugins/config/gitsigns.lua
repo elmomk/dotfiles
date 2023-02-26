c4b16af06dcd5392d68b94a6aa0ead0e65e2d75d
Salomo Bartholomees <salomob@gmail.com>
2023-02-26 18:00:00 +0100

    fix(gitsigns): use return

--- mo-vim/.config/nvim/lua/plugins/config/gitsigns.lua
+++ mo-vim/.config/nvim/lua/plugins/config/gitsigns.lua
@@ -1,9 +1,4 @@
--- local status_ok, gitsigns = pcall(require, "gitsigns")
--- if not status_ok then
--- 	return
--- end
-
-options = {
+return {
 	signs = {
 		add = { hl = "GitSignsAdd", text = "加", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
 		change = { hl = "GitSignsChange", text = "改", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
@@ -34,5 +29,3 @@ options = {
 		col = 1,
 	},
 }
-
-return options
