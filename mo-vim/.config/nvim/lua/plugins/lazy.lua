67817d1acacec2605437f98f16ef76aff9f9726c
Salomo Bartholomees <salomob@gmail.com>
2023-02-26 18:26:09 +0100

    bundle all keymaps together

--- mo-vim/.config/nvim/lua/plugins/lazy.lua
+++ mo-vim/.config/nvim/lua/plugins/lazy.lua
@@ -324,8 +324,8 @@ local plugins = {
 		opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help" } },
     -- stylua: ignore
     keys = {
-      { "<leader>qs", function() require("persistence").load() end,                desc = "Restore Session" },
-      { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+      -- { "<leader>qs", function() require("persistence").load() end,                desc = "Restore Session" },
+      -- { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
       { "<leader>qd", function() require("persistence").stop() end,                desc = "Don't Save Current Session" },
     },
 	},
