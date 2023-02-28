4789652072b57fa538e2810710357f97b1a55e74
Salomo Bartholomees <salomob@gmail.com>
2023-02-28 22:05:57 +0100

    add yaml companion

--- mo-vim/.config/nvim/lua/plugins/lazy.lua
+++ mo-vim/.config/nvim/lua/plugins/lazy.lua
@@ -102,6 +102,16 @@ local plugins = {
 		end,
 	},
 
+{"someone-stole-my-name/yaml-companion.nvim",
+dependencies = {
+            { "neovim/nvim-lspconfig" },
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-telescope/telescope.nvim" },
+    },
+    config = function()
+      require("telescope").load_extension("yaml_schema")
+    end,
+  },
 	-- The completion plugin
 	{
 		"hrsh7th/nvim-cmp",
