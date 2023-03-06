a7f6784b87806a54f5d61424e3ee4a85de86d8fa
Salomo Bartholomees <salomob@gmail.com>
2023-03-06 17:59:29 +0100

    Portal jumplist

--- mo-vim/.config/nvim/lua/plugins/keymaps.lua
+++ mo-vim/.config/nvim/lua/plugins/keymaps.lua
@@ -43,6 +43,8 @@ keymap("n", "<C-Down>", ":resize +2<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
+-- portal
+keymap("n", "<leader>p", ":Portal jumplist<CR>", opts)
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
