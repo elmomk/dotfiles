74a21ffb6ab063c846a88393c69f1c606514c0d0
Salomo Bartholomees <salomob@gmail.com>
2024-02-27 23:14:58 +0100

    Update

--- mo-vim/.config/nvim/lua/plugins/lazy.lua
+++ mo-vim/.config/nvim/lua/plugins/lazy.lua
@@ -168,12 +168,16 @@ local plugins = {
 		-- python -m venv venv
 		-- pip install python-dotenv requests pynvim==0.5.0 prompt-toolkit tiktoken
 		opts = {
-			show_help = "yes", -- Show help text for CopilotChatInPlace, default: yes
+			show_help = "no", -- Show help text for CopilotChatInPlace, default: yes
 			debug = false, -- Enable or disable debug mode, the log file will be in ~/.local/state/nvim/CopilotChat.nvim.log
 			disable_extra_info = "no", -- Disable extra information (e.g: system prompt) in the response.
 			language = "English", -- Copilot answer language settings when using default prompts. Default language is English.
 			-- proxy = "socks5://127.0.0.1:3000", -- Proxies requests via https or socks.
 			-- temperature = 0.1,
+      prompts = { -- Set dynamic prompts for CopilotChat commands
+        Explain = 'Explain how it works.',
+        Tests = 'Gnerate unit tests.',
+      }
 		},
 		build = function()
 			vim.notify("Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim.")
