86cf0c1c5fcef175092fb6b13b63ed1d93d28f3c
Salomo Bartholomees <salomob@gmail.com>
2025-06-19 19:51:55 +0800

    chore

--- mo-vim/.config/nvim/lua/plugins/lazy.lua
+++ mo-vim/.config/nvim/lua/plugins/lazy.lua
@@ -333,114 +333,114 @@ local plugins = {
       { "<leader>st", "<cmd>TodoTelescope<cr>",                            desc = "Todo" },
     },
 	},
-	{
-		"folke/noice.nvim",
-		event = "VeryLazy",
-		opts = {
-			lsp = {
-				override = {
-					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-					["vim.lsp.util.stylize_markdown"] = true,
-					["cmp.entry.get_documentation"] = true,
-				},
-			},
-			presets = {
-				bottom_search = true,
-				command_palette = true,
-				long_message_to_split = true,
-				inc_rename = true,
-				lsp_doc_border = true,
-				cmdline_output_to_split = true,
-			},
-		},
-		keys = {
-			{
-				"<S-Enter>",
-				function()
-					require("noice").redirect(vim.fn.getcmdline())
-				end,
-				mode = "c",
-				desc = "Redirect",
-			},
-			{
-				"<leader>snl",
-				function()
-					require("noice").cmd("last")
-				end,
-				desc = "Noice Last Message",
-			},
-			{
-				"<leader>snh",
-				function()
-					require("noice").cmd("history")
-				end,
-				desc = "Noice History",
-			},
-			{
-				"<leader>sna",
-				function()
-					require("noice").cmd("all")
-				end,
-				desc = "Noice All",
-			},
-			{
-				"<c-f>",
-				function()
-					if not require("noice.lsp").scroll(4) then
-						return "<c-f>"
-					end
-				end,
-				silent = true,
-				expr = true,
-				desc = "Scroll forward",
-				mode = {
-					"i",
-					"n",
-					"s",
-				},
-			},
-			{
-				"<c-b>",
-				function()
-					if not require("noice.lsp").scroll(-4) then
-						return "<c-b>"
-					end
-				end,
-				silent = true,
-				expr = true,
-				desc = "Scroll forward",
-				mode = {
-					"i",
-					"n",
-					"s",
-				},
-			},
-		},
-		dependencies = {
-			{
-				"rcarriga/nvim-notify",
-				keys = {
-					{
-						"<leader>n",
-						function()
-							require("notify").dismiss({ silent = true, pending = true })
-						end,
-						desc = "Delete all Notifications",
-					},
-				},
-				opts = {
-					timeout = 3000,
-					max_height = function()
-						return math.floor(vim.o.lines * 0.75)
-					end,
-					max_width = function()
-						return math.floor(vim.o.columns * 0.75)
-					end,
-				},
-			},
-		},
-	},
-
+--	{
+--		"folke/noice.nvim",
+--		event = "VeryLazy",
+--		opts = {
+--			lsp = {
+--				override = {
+--					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+--					["vim.lsp.util.stylize_markdown"] = true,
+--					["cmp.entry.get_documentation"] = true,
+--				},
+--			},
+--			presets = {
+--				bottom_search = true,
+--				command_palette = true,
+--				long_message_to_split = true,
+--				inc_rename = true,
+--				lsp_doc_border = true,
+--				cmdline_output_to_split = true,
+--			},
+--		},
+--		keys = {
+--			{
+--				"<S-Enter>",
+--				function()
+--					require("noice").redirect(vim.fn.getcmdline())
+--				end,
+--				mode = "c",
+--				desc = "Redirect",
+--			},
+--			{
+--				"<leader>snl",
+--				function()
+--					require("noice").cmd("last")
+--				end,
+--				desc = "Noice Last Message",
+--			},
+--			{
+--				"<leader>snh",
+--				function()
+--					require("noice").cmd("history")
+--				end,
+--				desc = "Noice History",
+--			},
+--			{
+--				"<leader>sna",
+--				function()
+--					require("noice").cmd("all")
+--				end,
+--				desc = "Noice All",
+--			},
+--			{
+--				"<c-f>",
+--				function()
+--					if not require("noice.lsp").scroll(4) then
+--						return "<c-f>"
+--					end
+--				end,
+--				silent = true,
+--				expr = true,
+--				desc = "Scroll forward",
+--				mode = {
+--					"i",
+--					"n",
+--					"s",
+--				},
+--			},
+--			{
+--				"<c-b>",
+--				function()
+--					if not require("noice.lsp").scroll(-4) then
+--						return "<c-b>"
+--					end
+--				end,
+--				silent = true,
+--				expr = true,
+--				desc = "Scroll forward",
+--				mode = {
+--					"i",
+--					"n",
+--					"s",
+--				},
+--			},
+--		},
+--		dependencies = {
+--			{
+--				"rcarriga/nvim-notify",
+--				keys = {
+--					{
+--						"<leader>n",
+--						function()
+--							require("notify").dismiss({ silent = true, pending = true })
+--						end,
+--						desc = "Delete all Notifications",
+--					},
+--				},
+--				opts = {
+--					timeout = 3000,
+--					max_height = function()
+--						return math.floor(vim.o.lines * 0.75)
+--					end,
+--					max_width = function()
+--						return math.floor(vim.o.columns * 0.75)
+--					end,
+--				},
+--			},
+--		},
+--	},
+--
 	-- session management
 	{
 		"folke/persistence.nvim",
