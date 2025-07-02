2e825fb73ea7258e95a6273c98ed7e968d8b9083
Salomo Bartholomees <salomob@gmail.com>
2025-07-02 13:18:14 +0800

    Feat: adding calendar and clean up

--- scripts/bin/cht.sh
+++ scripts/bin/cht.sh
@@ -6,7 +6,7 @@
 # This script is used to generate the cheat sheet for the cht.sh project.
 languages=$(echo "golang lua bash python rust shell" | tr ' ' '\n' | sort)
 # core_utils=$(echo "$(/usr/bin/ls /usr/bin) az rsync  distrobox fd ripgrep pacman npm yay tmux vim nvim awk sed grep find sort uniq wc xargs bat fzf mv rm ssh systemctl docker kubectl terraform ansible scp" | tr ' ' '\n' | sort)
-core_utils=$(echo "git tr cut useradd usermod groupmod groupadd paru helm az rsync  distrobox fd ripgrep pacman npm yay tmux vim nvim awk sed grep find sort uniq wc xargs bat fzf mv rm ssh systemctl docker kubectl terraform ansible scp" | tr ' ' '\n' | sort)
+core_utils=$(echo "nmcli dig git tr cut useradd usermod groupmod groupadd paru helm az rsync  distrobox fd ripgrep pacman npm yay tmux vim nvim awk sed grep find sort uniq wc xargs bat fzf mv rm ssh systemctl docker kubectl terraform ansible scp" | tr ' ' '\n' | sort)
 # core_utils=$(echo "paru useradd az rsync  distrobox fd ripgrep pacman npm yay tmux vim nvim awk sed grep find sort uniq wc xargs bat fzf mv rm ssh systemctl docker kubectl terraform ansible scp" | tr ' ' '\n' | sort)
 
 selected=$(printf '%s\n%s' "$languages" "$core_utils" | fzf --preview 'curl -s cht.sh/{}' --preview-window=right:75%:wrap)
@@ -15,12 +15,14 @@ read -rp "Enter topic: " query
 
 if printf '%s' "$languages" | grep -qs "$selected"; then
   echo "https://cht.sh/$selected/$query"
-  curl cht.sh/$selected/$query 2>/dev/null | bat & while [ : ]; do sleep 1; done
+  curl cht.sh/$selected/$query 2>/dev/null | bat &
+  while [ : ]; do sleep 1; done
   # tmux neww bash -c "curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
   # tmux display-popup -E "curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
 else
-    echo "https://cht.sh/$selected~$query"
-  curl cht.sh/$selected~$query 2>/dev/null | bat & while [ : ]; do sleep 1; done
+  echo "https://cht.sh/$selected~$query"
+  curl cht.sh/$selected~$query 2>/dev/null | bat &
+  while [ : ]; do sleep 1; done
   # tmux neww bash -c "curl cht.sh/$selected~$query & while [ : ]; do sleep 1; done"
   # tmux display-popup -E "curl cht.sh/$selected~$query & while [ : ]; do sleep 1; done"
 fi
