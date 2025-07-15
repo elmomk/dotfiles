6a883808f9e25cb656a4c0c51c8f3e29cdd9f176
Salomo Bartholomees <salomob@gmail.com>
2025-07-15 11:56:38 +0800

    Feat(brave): open line

---
+++ scripts/bin/brave_line.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -xe
+
+# hyprctl clients | grep "title: LINE" -A5 | grep initialTitle
+#  initialTitle: _crx_ophjlpahpchlmihnnnihgmmeilfjmjjc
+#  delete _crc_ and
+
+brave --app=chrome-extension://ophjlpahpchlmihnnnihgmmeilfjmjjc/index.html
