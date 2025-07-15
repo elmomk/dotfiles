393a3d87b7aaf1b46783a420df1ae6fcd2dcb59b
Salomo Bartholomees <salomob@gmail.com>
2025-07-15 15:10:27 +0800

    Feat(hyprland): add flameshot like behavior

--- hyprland/.config/hypr/install.sh
+++ hyprland/.config/hypr/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo pacman -S hyprland hypridle hyprlock hyprshot swaync waybar wofi hyprpolkitagent
+sudo pacman -S hyprland hypridle hyprlock hyprshot swaync waybar wofi hyprpolkitagent swappy grim slurp thunar pavucontrol swaync-client hyprshot wlogout wofi
 yay -S wlogout
