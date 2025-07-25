bb24d69cd83e3a784fa74dd9d44bf75e6f394c5b
Salomo Bartholomees <salomob@gmail.com>
2025-07-25 16:35:41 +0800

    Feat(clipboard): add clipboard manager

--- hyprland/.config/hypr/install.sh
+++ hyprland/.config/hypr/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo pacman -S hyprland hypridle hyprlock hyprshot swaync waybar wofi hyprpolkitagent swappy grim slurp thunar pavucontrol swaync-client hyprshot wlogout wofi
+sudo pacman -S hyprland hypridle hyprlock hyprshot swaync waybar hyprpolkitagent swappy grim slurp thunar pavucontrol swaync-client hyprshot wlogout wofi cliphist wl-copy
 yay -S wlogout
