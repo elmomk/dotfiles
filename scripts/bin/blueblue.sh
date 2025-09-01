dbc82e02bf7ee878ec529ee58d4ef08aaf15fd84
Salomo Bartholomees <salomob@gmail.com>
2025-09-01 10:48:19 +0800

    Feat(bluetooth): add bluetooth toggle for earbuds

--- scripts/bin/blueblue.sh
+++ scripts/bin/blueblue.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -xe
 
 POWER=$(bluetoothctl show | grep -i powerstate | awk '{print $NF}')
+BLOCK=$(rfkill list | grep -i hci0 -A1 | grep Soft | awk '{print $NF}')
+
+if [[ "$BLOCK" == "yes" ]]; then
+  sudo rfkill unblock bluetooth
+  notify-send "Bluetooth" "rfkill unblock bluetooth first"
+fi
 
 if [[ "$POWER" == "on" ]]; then
   bluetoothctl power off
