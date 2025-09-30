05f4b3be763f84b4a28fa403301be4ade1a50caa
Salomo Bartholomees <salomob@gmail.com>
2025-09-30 16:00:33 +0800

    feat(taskwarrior): try out new tools timewarrior and taskwarrior

--- hyprland/.config/hypr/install.sh
+++ hyprland/.config/hypr/install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 sudo pacman -S hyprland hypridle hyprlock hyprshot swaync waybar hyprpolkitagent swappy grim slurp thunar pavucontrol swaync-client hyprshot wlogout wofi cliphist wl-copy
+# sudo pacman -S timew
+# https://timewarrior.net/docs/
+# sudo pacman -SS taskwarrior
+# https://github.com/kdheepak/taskwarrior-tui
 yay -S wlogout
